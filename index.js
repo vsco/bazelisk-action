d2d062952ef974e08b1a6bb8897fd40809319f08
Aeternitaas <felidae@vsco.co>
2020-06-09 09:50:33 -0700

    Download bazelisk package func.

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
-const io = require('@actions/io');
-const tc = require('@actions/tool-cache');
-const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
 const run = require('./src/index.js')
 
 (async => {
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const io = require('@actions/io');
+const tc = require('@actions/tool-cache');
+const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
+
+async function run() {
+  try {
+    const version = core.getInput('version', { required : true });
+    const bazeliskPath = 
+      await tc.downloadTool(`https://github.com/bazelbuild/bazel/releases/download/${version}/bazel_${version}-linux-x86_64.deb`)
+    await exec.exec('dpkg', ['--force-all', '-i', `bazel_${version}-linux-x86_64.deb`])
+  } catch (err) {
+    core.error(err);
+    throw new Error(err)
+  }
+}
+
+module.exports = run;
