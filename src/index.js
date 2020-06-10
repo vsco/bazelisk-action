d1073d992276659b9b095046ca4a366b064ccce1
Aeternitaas <felidae@vsco.co>
2020-06-10 12:47:17 -0700

    Cleaned up debug messages.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const tc = require('@actions/tool-cache');
 
 async function run() {
   try {
+    core.debug('Begin Bazelisk Action');
     const version =
       core.getInput('version', { required : true });
     const bazelBinPath =
@@ -12,16 +13,14 @@ async function run() {
 
     const bazeliskPath =
       await tc.downloadTool(`https://github.com/bazelbuild/bazelisk/releases/download/v${version}/bazelisk-linux-amd64`);
-    core.debug('Finished downloading to bazeliskPath');
+    core.debug('Successfully downloaded binary to bazeliskPath');
 
     // Create directory, move into directory, chmod +x bazel, and add to path.
     await io.mkdirP(bazelBinPath);
     await io.mv(bazeliskPath, `${bazelBinPath}/bazel`);
     await exec.exec('chmod', ['+x', `${bazelBinPath}/bazel`]);
-    core.debug('Chmodded!')
     await core.addPath(`${bazelBinPath}/bazel`);
-    core.debug('Added to path:\n');
-    core.debug(process.env.PATH);
+    core.debug(`Added ${bazelBinPath}/bazel to PATH`);
     
   } catch (err) {
     core.error(err);
