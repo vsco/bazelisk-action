93cbb13de3cf3ca6c9c02f177f5ff0480ea06274
Aeternitaas <felidae@vsco.co>
2020-06-11 14:08:18 -0700

    Rebuilt.

--- dist/index.js
+++ dist/index.js
@@ -4712,6 +4712,7 @@ const tc = __webpack_require__(533);
 
 async function run() {
   try {
+    core.debug('Begin Bazelisk Action');
     const version =
       core.getInput('version', { required : true });
     const bazelBinPath =
@@ -4719,16 +4720,14 @@ async function run() {
 
     const bazeliskPath =
       await tc.downloadTool(`https://github.com/bazelbuild/bazelisk/releases/download/v${version}/bazelisk-linux-amd64`);
-    core.debug('Finished downloading to bazeliskPath');
+    core.debug('Successfully downloaded binary to bazeliskPath');
 
     // Create directory, move into directory, chmod +x bazel, and add to path.
     await io.mkdirP(bazelBinPath);
     await io.mv(bazeliskPath, `${bazelBinPath}/bazel`);
     await exec.exec('chmod', ['+x', `${bazelBinPath}/bazel`]);
-    core.debug('Chmodded!')
-    await core.addPath(`${bazelBinPath}/bazel`);
-    core.debug('Added to path:\n');
-    core.debug(process.env.PATH);
+    await core.addPath(`${bazelBinPath}`);
+    core.debug(`Added ${bazelBinPath}/bazel to PATH`);
     
   } catch (err) {
     core.error(err);
